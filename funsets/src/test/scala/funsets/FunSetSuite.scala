7e167a14c43fdbe2f41f418b044ee40de42b04b8
Krzysztof Zielinski <Krzysztof.zielinski@sainsburys.co.uk>
2018-03-04 10:57:03 +0100

    update

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -29,10 +29,10 @@ class FunSetSuite extends FunSuite {
   /**
    * Tests are written using the "test" operator and the "assert" method.
    */
-  // test("string take") {
-  //   val message = "hello, world"
-  //   assert(message.take(5) == "hello")
-  // }
+  test("string take") {
+     val message = "hello, world"
+     assert(message.take(5) == "hello")
+   }
 
   /**
    * For ScalaTest tests, there exists a special equality operator "===" that
@@ -43,9 +43,9 @@ class FunSetSuite extends FunSuite {
    * Try it out! Change the values so that the assertion fails, and look at the
    * error message.
    */
-  // test("adding ints") {
-  //   assert(1 + 2 === 3)
-  // }
+  test("adding ints") {
+    assert(1 + 2 === 3)
+  }
 
 
   import FunSets._
@@ -110,5 +110,13 @@ class FunSetSuite extends FunSuite {
     }
   }
 
+  test("intersection returns of the two given sets") {
+    new TestSets {
+      val s = intersect(s1, s2)
+      assert(!contains(s, 1), "Inter 1")
+      assert(!contains(s, 2), "Inter 2")
+      assert(!contains(s, 3), "Inter 3")
+    }
+  }
 
 }
